c49f89ebaf57e8c2bf8c661c56978d183003b591
Silent Bob <necr@tut.by>
2014-06-30 18:32:35 +0300

    revert 3

--- script/tracker/backgroundWorker.js
+++ script/tracker/backgroundWorker.js
@@ -7,7 +7,6 @@ BP.tracker.BackgroundWorker = can.Construct.extend({}, {
     init: function () {
         this.model = BP.model.Parcel;
         this.start();
-        "string from master"
     },
 
     start: function() {
