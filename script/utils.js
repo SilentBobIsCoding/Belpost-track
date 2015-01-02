23c0fd59b4ee9368ea227282d49fb346ecff8e25
Silent Bob <necr@tut.by>
2015-01-02 19:04:28 +0300

    Fix year calculation

--- script/utils.js
+++ script/utils.js
@@ -1,7 +1,7 @@
 BP.utils.dateFormatter = function (date) {
     var res = '';
     if (date instanceof Date) {
-        var parts = [date.getDate(), date.getMonth() + 1, date.getYear() - 100];
+        var parts = [date.getDate(), date.getMonth() + 1, date.getFullYear() % 100];
         parts.forEach(function (part, i) {
             parts[i] = (part < 10 ? '0' + part : part);
         });
